e5921e1aa399d7fb88c6d6b3d1a1460ffa4ffb3c
= <=>
2024-04-05 21:59:38 -0400

    Classwork Except Testing

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace UtilityLibraries;
+/// <summary>
+/// This class holds extension mehtods for the string data type
+/// </summary>
+public static class StringLibrary
+{
+    /// <summary>
+    /// Detects whether or not the first letter is an uppercase letter.
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
